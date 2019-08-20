a8edad6b8a08dae15fb54826b004ea8470d7e804
Wang Xian <wangxian_drew@163.com>
2019-08-20 09:20:25 +0800

    initial commit

---
+++ src/main/java/org/ziyan/cloud/oauth2/CloudOauth2Application.java
@@ -0,0 +1,19 @@
+package org.ziyan.cloud.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序启动类
+ * 
+ * @author wangx
+ *
+ */
+@SpringBootApplication
+public class CloudOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudOauth2Application.class, args);
+	}
+
+}
